5e0e1aaf9855d8c2a6a3de17174bfc86809f9f34
joaonotfound <joaonotfound@protonmail.com>
2023-03-10 22:56:49 -0300

    update script to automtically configure nginx

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,3 +1,5 @@
 sudo su -
+sed -i "s/yourdomain.com/$DOMAIN/g" nginx/ghost
 cp ./nginx/ghost /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/ghost /etc/nginx/sites-enabled/ghost
+/etc/init.d/nginx restart
